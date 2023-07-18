b8305bf88235207f7565562f1d9bdfe7c05ceef8
Mikhail Makavetski <zombisuki@gmail.com>
2023-07-18 19:45:41 +0200

    add default pagination

---
+++ resources/js/Components/Pagination.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="flex">
+        <template v-for="(link, key) in links" :key="key">
+            <div
+                v-if="link.url === null"
+                class="mb-1 mr-1 px-4 py-3 text-gray-400 text-sm leading-4 border rounded"
+                v-html="link.label">
+
+            </div>
+            <Link
+                v-else
+                :class="{'bg-indigo-200': link.active}"
+                :href="link.url"
+                class="mr-1 mr-1 px-4 py-3 text-sm leading-4 border rounded focus:text-indigo-500 focus:border-indigo-500 hover:bg-white"
+                v-html="link.label"/>
+        </template>
+    </div>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3';
+import { defineProps } from 'vue';
+
+defineProps({
+    links: Array,
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- resources/js/Pages/Tags/Index.vue
+++ resources/js/Pages/Tags/Index.vue
@@ -23,7 +23,8 @@
                                 <div class="flex-1">
                                     <div class="relative">
                                         <div class="absolute flex items-center ml-2 h-full">
-                                            <svg class="w-4 h-4 fill-current text-primary-gray-dark" viewBox="0 0 16 16" fill="none"
+                                            <svg class="w-4 h-4 fill-current text-primary-gray-dark" fill="none"
+                                                 viewBox="0 0 16 16"
                                                  xmlns="http://www.w3.org/2000/svg">
                                                 <path
                                                     d="M15.8898 15.0493L11.8588 11.0182C11.7869 10.9463 11.6932 10.9088 11.5932 10.9088H11.2713C12.3431 9.74952 12.9994 8.20272 12.9994 6.49968C12.9994 2.90923 10.0901 0 6.49968 0C2.90923 0 0 2.90923 0 6.49968C0 10.0901 2.90923 12.9994 6.49968 12.9994C8.20272 12.9994 9.74952 12.3431 10.9088 11.2744V11.5932C10.9088 11.6932 10.9495 11.7869 11.0182 11.8588L15.0493 15.8898C15.1961 16.0367 15.4336 16.0367 15.5805 15.8898L15.8898 15.5805C16.0367 15.4336 16.0367 15.1961 15.8898 15.0493ZM6.49968 11.9994C3.45921 11.9994 0.999951 9.54016 0.999951 6.49968C0.999951 3.45921 3.45921 0.999951 6.49968 0.999951C9.54016 0.999951 11.9994 3.45921 11.9994 6.49968C11.9994 9.54016 9.54016 11.9994 6.49968 11.9994Z">
@@ -31,13 +32,16 @@
                                             </svg>
                                         </div>
 
-                                        <input wire:model="search" type="text" placeholder="Search by title"
-                                               class="px-8 py-3 w-full md:w-2/6 rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm" />
+                                        <input
+                                            class="px-8 py-3 w-full md:w-2/6 rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
+                                            placeholder="Search by title" type="text"
+                                            wire:model="search"/>
                                     </div>
                                 </div>
                                 <div class="flex">
-                                    <select wire:model="perPage"
-                                            class="px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm">
+                                    <select
+                                        class="px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
+                                        wire:model="perPage">
                                         <option value="5">5 Per Page</option>
                                         <option value="10">10 Per Page</option>
                                         <option value="15">15 Per Page</option>
@@ -75,7 +79,7 @@
 
                             </table>
                             <div class="m-2 p-2">
-                                links
+<Pagination :links="tags.links"/>
                             </div>
                         </div>
                     </div>
@@ -88,6 +92,7 @@
 <script setup>
 import AdminLayout from '@/Layouts/AdminLayout.vue';
 import { Link } from '@inertiajs/vue3';
+import Pagination from '@/Components/Pagination.vue';
 
 const props = defineProps({
     tags: Object,
